b2eca06e05e7567e562509fc01b4678336f5221e
Jose Fernan Banguera Villalobo <jose.banguera@PRAGMA.COM.CO>
2018-05-10 11:08:25 -0500

    Creacion de user para pagina principal, localstorage

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    this.grabarUser();
+    this.obtenerUser();
+  }
 
   ngOnInit() {
+  }
+
+  grabarUser(){
+    let user = [
+      {
+        nombre:"Josef",
+        name:"josefpk",
+        password: 123456,
+        email:"jose@jose"
+        },
+      {
+        nombre:"Felix",
+        name:"felixxiam",
+        password: 123456,
+        email:"felix@felix"
+      }
+    ]
+
+    localStorage.setItem("user", JSON.stringify(user));
+  }
+
+  obtenerUser(){
+    let user = JSON.parse(localStorage.getItem("user"));
   }
 
 }
